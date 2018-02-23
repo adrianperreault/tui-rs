dabb738362594a294a63b58fb39b3e828f49f00e
Adrian Perreault <eladriano@gmail.com>
2018-02-23 13:12:23 -0800

    Add Double, Dashed, Heavy lines + Quadrant blocks

--- src/symbols.rs
+++ src/symbols.rs
@@ -7,6 +7,10 @@ pub mod block {
     pub const THREE_EIGHTHS: &str = "▍";
     pub const ONE_QUATER: &str = "▎";
     pub const ONE_EIGHTH: &str = "▏";
+
+    pub const FULL_LIGHT_SHADE: &str = "░";
+    pub const FULL_MEDIUM_SHADE: &str = "▒";
+    pub const FULL_DARK_SHADE: &str = "▓";
 }
 
 pub mod bar {
@@ -31,6 +35,155 @@ pub mod line {
     pub const VERTICAL_RIGHT: &str = "├";
     pub const HORIZONTAL_DOWN: &str = "┬";
     pub const HORIZONTAL_UP: &str = "┴";
+
+    pub const DOUBLE_DOWN_AND_HORIZONTAL: &str = "╦";
+    pub const DOUBLE_DOWN_AND_LEFT: &str = "╗";
+    pub const DOUBLE_DOWN_AND_RIGHT: &str = "╔";
+    pub const DOUBLE_HORIZONTAL: &str = "═";
+    pub const DOUBLE_UP_AND_HORIZONTAL: &str = "╩";
+    pub const DOUBLE_UP_AND_LEFT: &str = "╝";
+    pub const DOUBLE_UP_AND_RIGHT: &str = "╚";
+    pub const DOUBLE_VERTICAL: &str = "║";
+    pub const DOUBLE_VERTICAL_AND_HORIZONTAL: &str = "╬";
+    pub const DOUBLE_VERTICAL_AND_LEFT: &str = "╣";
+    pub const DOUBLE_VERTICAL_AND_RIGHT: &str = "╠";
+    pub const DOWN_DOUBLE_AND_HORIZONTAL_SINGLE: &str = "╥";
+    pub const DOWN_DOUBLE_AND_LEFT_SINGLE: &str = "╖";
+    pub const DOWN_DOUBLE_AND_RIGHT_SINGLE: &str = "╓";
+
+    pub const DOWN_HEAVY_AND_HORIZONTAL_LIGHT: &str = "┰";
+    pub const DOWN_HEAVY_AND_LEFT_LIGHT: &str = "┒";
+    pub const DOWN_HEAVY_AND_LEFT_UP_LIGHT: &str = "┧";
+    pub const DOWN_HEAVY_AND_RIGHT_LIGHT: &str = "┎";
+    pub const DOWN_HEAVY_AND_RIGHT_UP_LIGHT: &str = "┟";
+    pub const DOWN_HEAVY_AND_UP_HORIZONTAL_LIGHT: &str = "╁";
+    pub const DOWN_LIGHT_AND_HORIZONTAL_HEAVY: &str = "┯";
+    pub const DOWN_LIGHT_AND_LEFT_HEAVY: &str = "┑";
+    pub const DOWN_LIGHT_AND_LEFT_UP_HEAVY: &str = "┩";
+    pub const DOWN_LIGHT_AND_RIGHT_HEAVY: &str = "┍";
+    pub const DOWN_LIGHT_AND_RIGHT_UP_HEAVY: &str = "┡";
+    pub const DOWN_LIGHT_AND_UP_HORIZONTAL_HEAVY: &str = "╇";
+    pub const DOWN_SINGLE_AND_HORIZONTAL_DOUBLE: &str = "╤";
+    pub const DOWN_SINGLE_AND_LEFT_DOUBLE: &str = "╕";
+    pub const DOWN_SINGLE_AND_RIGHT_DOUBLE: &str = "╒";
+
+    pub const HEAVY_DOUBLE_DASH_HORIZONTAL: &str = "╍";
+    pub const HEAVY_DOUBLE_DASH_VERTICAL: &str = "╏";
+    pub const HEAVY_DOWN: &str = "╻";
+    pub const HEAVY_DOWN_AND_HORIZONTAL: &str = "┳";
+    pub const HEAVY_DOWN_AND_LEFT: &str = "┓";
+    pub const HEAVY_DOWN_AND_RIGHT: &str = "┏";
+    pub const HEAVY_HORIZONTAL: &str = "━";
+    pub const HEAVY_LEFT: &str = "╸";
+    pub const HEAVY_LEFT_AND_LIGHT_RIGHT: &str = "╾";
+    pub const HEAVY_QUADRUPLE_DASH_HORIZONTAL: &str = "┉";
+    pub const HEAVY_QUADRUPLE_DASH_VERTICAL: &str = "┋";
+    pub const HEAVY_RIGHT: &str = "╺";
+    pub const HEAVY_TRIPLE_DASH_HORIZONTAL: &str = "┅";
+    pub const HEAVY_TRIPLE_DASH_VERTICAL: &str = "┇";
+    pub const HEAVY_UP: &str = "╹";
+    pub const HEAVY_UP_AND_HORIZONTAL: &str = "┻";
+    pub const HEAVY_UP_AND_LEFT: &str = "┛";
+    pub const HEAVY_UP_AND_LIGHT_DOWN: &str = "╿";
+    pub const HEAVY_UP_AND_RIGHT: &str = "┗";
+    pub const HEAVY_VERTICAL: &str = "┃";
+    pub const HEAVY_VERTICAL_AND_HORIZONTAL: &str = "╋";
+    pub const HEAVY_VERTICAL_AND_LEFT: &str = "┫";
+    pub const HEAVY_VERTICAL_AND_RIGHT: &str = "┣";
+
+    pub const LEFT_DOWN_HEAVY_AND_RIGHT_UP_LIGHT: &str = "╅";
+    pub const LEFT_HEAVY_AND_RIGHT_DOWN_LIGHT: &str = "┭";
+    pub const LEFT_HEAVY_AND_RIGHT_UP_LIGHT: &str = "┵";
+    pub const LEFT_HEAVY_AND_RIGHT_VERTICAL_LIGHT: &str = "┽";
+    pub const LEFT_LIGHT_AND_RIGHT_DOWN_HEAVY: &str = "┲";
+    pub const LEFT_LIGHT_AND_RIGHT_UP_HEAVY: &str = "┺";
+    pub const LEFT_LIGHT_AND_RIGHT_VERTICAL_HEAVY: &str = "╊";
+    pub const LEFT_UP_HEAVY_AND_RIGHT_DOWN_LIGHT: &str = "╃";
+
+    pub const LIGHT_ARC_DOWN_AND_LEFT: &str = "╮";
+    pub const LIGHT_ARC_DOWN_AND_RIGHT: &str = "╭";
+    pub const LIGHT_ARC_UP_AND_LEFT: &str = "╯";
+    pub const LIGHT_ARC_UP_AND_RIGHT: &str = "╰";
+    pub const LIGHT_DIAGONAL_CROSS: &str = "╳";
+    pub const LIGHT_DIAGONAL_UPPER_LEFT_TO_LOWER_RIGHT: &str = "╲";
+    pub const LIGHT_DIAGONAL_UPPER_RIGHT_TO_LOWER_LEFT: &str = "╱";
+    pub const LIGHT_DOUBLE_DASH_HORIZONTAL: &str = "╌";
+    pub const LIGHT_DOUBLE_DASH_VERTICAL: &str = "╎";
+    pub const LIGHT_DOWN: &str = "╷";
+    pub const LIGHT_DOWN_AND_HORIZONTAL: &str = "┬";
+    pub const LIGHT_DOWN_AND_LEFT: &str = "┐";
+    pub const LIGHT_DOWN_AND_RIGHT: &str = "┌";
+    pub const LIGHT_HORIZONTAL: &str = "─";
+    pub const LIGHT_LEFT: &str = "╴";
+    pub const LIGHT_LEFT_AND_HEAVY_RIGHT: &str = "╼";
+    pub const LIGHT_QUADRUPLE_DASH_HORIZONTAL: &str = "┈";
+    pub const LIGHT_QUADRUPLE_DASH_VERTICAL: &str = "┊";
+    pub const LIGHT_RIGHT: &str = "╶";
+    pub const LIGHT_TRIPLE_DASH_HORIZONTAL: &str = "┄";
+    pub const LIGHT_TRIPLE_DASH_VERTICAL: &str = "┆";
+    pub const LIGHT_UP: &str = "╵";
+    pub const LIGHT_UP_AND_HEAVY_DOWN: &str = "╽";
+    pub const LIGHT_UP_AND_HORIZONTAL: &str = "┴";
+    pub const LIGHT_UP_AND_LEFT: &str = "┘";
+    pub const LIGHT_UP_AND_RIGHT: &str = "└";
+    pub const LIGHT_VERTICAL: &str = "│";
+    pub const LIGHT_VERTICAL_AND_HORIZONTAL: &str = "┼";
+    pub const LIGHT_VERTICAL_AND_LEFT: &str = "┤";
+    pub const LIGHT_VERTICAL_AND_RIGHT: &str = "├";
+
+    pub const RIGHT_DOWN_HEAVY_AND_LEFT_UP_LIGHT: &str = "╆";
+    pub const RIGHT_HEAVY_AND_LEFT_DOWN_LIGHT: &str = "┮";
+    pub const RIGHT_HEAVY_AND_LEFT_UP_LIGHT: &str = "┶";
+    pub const RIGHT_HEAVY_AND_LEFT_VERTICAL_LIGHT: &str = "┾";
+    pub const RIGHT_LIGHT_AND_LEFT_DOWN_HEAVY: &str = "┱";
+    pub const RIGHT_LIGHT_AND_LEFT_UP_HEAVY: &str = "┹";
+    pub const RIGHT_LIGHT_AND_LEFT_VERTICAL_HEAVY: &str = "╉";
+    pub const RIGHT_UP_HEAVY_AND_LEFT_DOWN_LIGHT: &str = "╄";
+
+    pub const UP_DOUBLE_AND_HORIZONTAL_SINGLE: &str = "╨";
+    pub const UP_DOUBLE_AND_LEFT_SINGLE: &str = "╜";
+    pub const UP_DOUBLE_AND_RIGHT_SINGLE: &str = "╙";
+    pub const UP_HEAVY_AND_DOWN_HORIZONTAL_LIGHT: &str = "╀";
+    pub const UP_HEAVY_AND_HORIZONTAL_LIGHT: &str = "┸";
+    pub const UP_HEAVY_AND_LEFT_DOWN_LIGHT: &str = "┦";
+    pub const UP_HEAVY_AND_LEFT_LIGHT: &str = "┚";
+    pub const UP_HEAVY_AND_RIGHT_DOWN_LIGHT: &str = "┞";
+    pub const UP_HEAVY_AND_RIGHT_LIGHT: &str = "┖";
+    pub const UP_LIGHT_AND_DOWN_HORIZONTAL_HEAVY: &str = "╈";
+    pub const UP_LIGHT_AND_HORIZONTAL_HEAVY: &str = "┷";
+    pub const UP_LIGHT_AND_LEFT_DOWN_HEAVY: &str = "┪";
+    pub const UP_LIGHT_AND_LEFT_HEAVY: &str = "┙";
+    pub const UP_LIGHT_AND_RIGHT_DOWN_HEAVY: &str = "┢";
+    pub const UP_LIGHT_AND_RIGHT_HEAVY: &str = "┕";
+    pub const UP_SINGLE_AND_HORIZONTAL_DOUBLE: &str = "╧";
+    pub const UP_SINGLE_AND_LEFT_DOUBLE: &str = "╛";
+    pub const UP_SINGLE_AND_RIGHT_DOUBLE: &str = "╘";
+
+    pub const VERTICAL_DOUBLE_AND_HORIZONTAL_SINGLE: &str = "╫";
+    pub const VERTICAL_DOUBLE_AND_LEFT_SINGLE: &str = "╢";
+    pub const VERTICAL_DOUBLE_AND_RIGHT_SINGLE: &str = "╟";
+    pub const VERTICAL_HEAVY_AND_HORIZONTAL_LIGHT: &str = "╂";
+    pub const VERTICAL_HEAVY_AND_LEFT_LIGHT: &str = "┨";
+    pub const VERTICAL_HEAVY_AND_RIGHT_LIGHT: &str = "┠";
+    pub const VERTICAL_LIGHT_AND_HORIZONTAL_HEAVY: &str = "┿";
+    pub const VERTICAL_LIGHT_AND_LEFT_HEAVY: &str = "┥";
+    pub const VERTICAL_LIGHT_AND_RIGHT_HEAVY: &str = "┝";
+    pub const VERTICAL_SINGLE_AND_HORIZONTAL_DOUBLE: &str = "╪";
+    pub const VERTICAL_SINGLE_AND_LEFT_DOUBLE: &str = "╡";
+    pub const VERTICAL_SINGLE_AND_RIGHT_DOUBLE: &str = "╞";
+}
+
+pub mod quadrant_blocks {
+    pub const QUADRANT_LOWER_LEFT: &str = "▖";
+    pub const QUADRANT_LOWER_RIGHT: &str = "▗";
+    pub const QUADRANT_UPPER_LEFT: &str = "▘";
+    pub const QUADRANT_UPPER_LEFT_AND_LOWER_LEFT_AND_LOWER_RIGHT: &str = "▙";
+    pub const QUADRANT_UPPER_LEFT_AND_LOWER_RIGHT: &str = "▚";
+    pub const QUADRANT_UPPER_LEFT_AND_UPPER_RIGHT_AND_LOWER_LEFT: &str = "▛";
+    pub const QUADRANT_UPPER_LEFT_AND_UPPER_RIGHT_AND_LOWER_RIGHT: &str = "▜";
+    pub const QUADRANT_UPPER_RIGHT: &str = "▝";
+    pub const QUADRANT_UPPER_RIGHT_AND_LOWER_LEFT: &str = "▞";
+    pub const QUADRANT_UPPER_RIGHT_AND_LOWER_LEFT_AND_LOWER_RIGHT: &str = "▟";
 }
 
 pub const DOT: &str = "•";
